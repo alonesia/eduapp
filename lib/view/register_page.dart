97f829de6d0f0849b37cbe68930bbc4b9cc67c66
Heri Purnomo <okejsb84@gmail.com>
2022-04-03 23:50:04 +0700

    Add register & Login

---
+++ lib/view/register_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: const Text(
+          "Yuk isi data diri",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [Text("Nama lengkap"), TextField()],
+        ),
+      ),
+    );
+  }
+}
