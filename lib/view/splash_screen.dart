c648ff6bfdabea5ddd7183cd5aa54c8c3cdca38f
Heri Purnomo <okejsb84@gmail.com>
2022-04-05 16:33:40 +0700

    Add timer splash

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,8 +1,29 @@
+import 'dart:async';
+
 import 'package:eduapps/view/login_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  goToNextPage() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const LoginPage(),
+      ));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    goToNextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
